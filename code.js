baa80559f88d11af36f567f2954b6339f7d9687d
Jonathan Hutson <jhut110766@gmail.com>
2017-01-10 15:50:18 -0500

    intro to javascript

---
+++ code.js
@@ -0,0 +1,29 @@
+// 1. Assign the message "Hello, World!" to a variable.
+var hello = "Hello, World";
+
+console.log(hello)
+
+// 2. Assign a different string to a variable name of your choice.
+
+var sentence = "this is a sentence.";
+
+console.log(sentence);
+
+// 3. Assign a number to a variable.
+
+var number = 8;
+
+console.log(number);
+
+// 4. Use string concatenation to display the number from #3 in a string.
+
+var num = 8;
+
+var n = num.toString();
+
+// 5. Make an array of at least four of your favorite movies or books or bands.
+var movies ["iron man", "iron will", "mighty ducks", "balto"]
+
+// 6. Make a object of information about yourself with at least four properties.
+
+var me = {firstName:"Jonathan", lastName:"Hutson", age:22, eyeColor:"brown"};
